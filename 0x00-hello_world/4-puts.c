af7f0d93d37ea6307a0b650a7a8953a8885a7525
MezieSamuel <meziedeworld@yahoo.com>
2023-03-17 16:56:39 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *main - the main function
- *return : always 0 (Success)
- */
+*main - the main function
+*return : always 0 (Success)
+*/
 int main(void)
 {	
 	puts("\"Programming is like buiding a multilingual puzzle");

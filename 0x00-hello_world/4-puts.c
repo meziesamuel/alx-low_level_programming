9640b5003794949628f1a791ee9940588ca738b2
MezieSamuel <meziedeworld@yahoo.com>
2023-03-18 05:38:12 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- * main - Access point
+ * main - Entry point
+ *
  * Return: 0
- */ 
-int main(void) 
-{
-	puts("\"Programming is like building a multilingual puzzle"); 
+ */
+
+int main(void)
+{	
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+}		

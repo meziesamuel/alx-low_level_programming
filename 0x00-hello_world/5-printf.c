c15392f45406db8f0420c6b6531397234708b370
MezieSamuel <meziedeworld@yahoo.com>
2023-03-18 07:01:15 -0700

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,13 @@
 #include <stdio>
+
 /**
- *main - Access point
- *Return: always 0 (success)
+ * main - Access point
+ *
+ * Return: 0
  */
+
 int main(void)
 {	
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
+}

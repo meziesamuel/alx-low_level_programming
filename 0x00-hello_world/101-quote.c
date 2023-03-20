b55fbe1da1b8033bde3a625fbe3f040c90d5fa3f
MezieSamuel <meziedeworld@yahoo.com>
2023-03-20 01:08:16 -0700

    Quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpa, 2015-10-19\n", stdout);
+	return (0);
+}	

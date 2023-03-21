c919395917548d45b776f786b7dfa4510a348bf5
MezieSamuel <meziedeworld@yahoo.com>
2023-03-21 06:56:17 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * main - Entry point
- * Description: 'prints _putchar to stdout'
+ * Description: 'prints all alphabets in _putchar'
  * Return: always 0
  */
 int main(void)
@@ -14,6 +14,5 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n');
 	return (0);
-}	
+}

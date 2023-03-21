ba55df462f4bf1ab51a00078659b456644b7ef29
MezieSamuel <meziedeworld@yahoo.com>
2023-03-21 09:05:50 -0700

    Alphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,15 +8,13 @@
 
 void print_alphabet_x10(void)
 {
-	char i;
+	int i;
 	char j;
 
 	for (j = 1; i <= 10; i++)
 	{
 		for (j = 97; j <= 122; j++)
-		{
 			_putchar(j);
-		}
 		_putchar('\n');
 	}
 }

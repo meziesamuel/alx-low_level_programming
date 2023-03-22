02f04f642a2b1d37dabd66d16c85c2b0688ebcfa
MezieSamuel <meziedeworld@yahoo.com>
2023-03-22 04:52:44 -0700

    Is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -9,9 +9,10 @@
  * and 0 if otherwise
  */
 
-int _isalpha(int c)
+int _islower(int c)
 {
-	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	if (c >= 97 && c <= 122)
 		return (1);
+	else
 	        return (0);
 }

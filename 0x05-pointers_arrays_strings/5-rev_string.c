e0a7692b06eb20fad717387a3e48dc96fd41bd5c
MezieSamuel <meziedeworld@yahoo.com>
2023-03-27 23:49:30 -0700

    Reversing a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: function parameter
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+		count++;
+	for (i = count ; i >= 0 ; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}

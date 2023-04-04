af1e5307065d01f0ac4784833f23e8b8c2da694d
MezieSamuel <meziedeworld@yahoo.com>
2023-04-04 16:34:54 -0700

    Print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints chessboard
+ * @a: array
+ * Reurn: 0
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 0; i++)
+	{
+		for (j = 0; j < 0; j++)
+			_putchar(a[i][j]);
+		_putchar('\0');
+	}
+}

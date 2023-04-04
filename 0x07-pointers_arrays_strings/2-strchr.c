7819ef2874e98a54e70b790efcd3064bcbd9344f
MezieSamuel <meziedeworld@yahoo.com>
2023-04-04 03:46:17 -0700

    Character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: the string
+ * @c: the character to be located
+ * Return: 0
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+}

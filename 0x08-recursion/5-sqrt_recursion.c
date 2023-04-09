df653f86ea2a39286dbc4cda2efdd4dbb63e0b85
MezieSamuel <meziedeworld@yahoo.com>
2023-04-09 12:05:25 -0700

    Program that prints its name

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to find square root of
+ * Return: square root of n
+ * -1 if n does not have a natural square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else
+		return (n, 0);
+}
---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - prints name of program
+ * @argc: number of arguments
+ * @argv: array of integers
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}

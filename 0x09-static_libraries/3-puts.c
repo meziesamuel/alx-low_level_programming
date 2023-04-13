02d702162d0cfc0db921d9aad9c7ad7ac3771540
MezieSamuel <meziedeworld@yahoo.com>
2023-04-13 08:07:23 -0700

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - Entry point
+ * @str: function parameter
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str)
+{
+int i = 0;
+while (str[i])
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
+}
+
